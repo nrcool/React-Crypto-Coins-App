c792f956a723e46c788004c389baea5fdfdc5564
nrcool <nomanalam7214@gmail.com>
2022-02-20 20:02:35 +0100

    increase banner height

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const useStyles=makeStyles(()=>({
         backgroundImage:"url(./banner.gif)",
     },
     bannerContent:{
-        height:400,
+        height:500,
         display:"flex",
         flexDirection:"column",
         paddingTop:25,
