b0d9cc08777b8bc9b68793b095de1d46e4b3b55d
nrcool <nomanalam7214@gmail.com>
2022-02-20 18:11:18 +0100

    homepage component displaying banner and coinstable comp

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './Banner'
+import CoinsTable from './CoinsTable'
+
+export default function HomePage() {
+  return (
+    <>
+    <Banner/>
+    <CoinsTable/>
+    </>
+  )
+}
